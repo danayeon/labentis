015dec354ce609ef94a7b18f5bdd0a77ae02ced0
danayeon <tigerwest1009@gmail.com>
2020-05-10 16:55:49 +0900

    generate project-model

---
+++ db/migrate/20200510075308_create_projects.rb
@@ -0,0 +1,15 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.integer :team_id, null: false, foreign_key: true
+      t.integer :product_id, null: false, foreign_key: true
+      t.text :title, null: false
+      t.text :url, null: false
+      t.text :overview, null: false
+      t.integer :reported
+      t.integer :alert
+      t.datetime :upload_at, null: false
+      t.timestamps
+    end
+  end
+end
