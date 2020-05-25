11858662b98e85520edf270d8bffd647b84aad40
danayeon <tigerwest1009@gmail.com>
2020-05-25 14:40:16 +0900

    erb2haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
       g.helper false
       g.test_framework false
     end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, ])
+    #deviseにて使用できるdevise_parameter_sanitizerのpermitメソッドがストロングパラメーターにあたる
+    #上記ではサインアップ時に入力されたnicknameキーの内容の保存を許可している
+  end
 end
