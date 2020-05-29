ad0b5ce81b3daef3c97fa14a82873b0fa50f9308
danayeon <tigerwest1009@gmail.com>
2020-05-29 17:24:12 +0900

    remake projects table

--- db/migrate/20200526182539_create_projects.rb
+++ db/migrate/20200526182539_create_projects.rb
@@ -1,14 +1,25 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-      t.integer :product_id, null: false, foreign_key: true
-      t.text :title, null: false
-      t.text :url, null: false
-      t.text :overview, null: false
-      t.integer :reported
-      t.integer :alert
-      t.datetime :upload_at, null: false
+      t.string      :title,       null: false
+      t.text        :statement,   null: false
+      t.references  :track_user
+      t.references  :lyric_user
+      t.references  :singer_user
+      t.references  :video_user
+      t.references  :manage_usre
+      t.text        :link
+      t.text        :overview
+      t.datetime    :upload_at
+      t.integer     :reported
+      t.integer     :alert
       t.timestamps
     end
+
+    add_foreign_key :projects, :users, column: :track_user_id
+    add_foreign_key :projects, :users, column: :lyric_user_id
+    add_foreign_key :projects, :users, column: :singer_user_id
+    add_foreign_key :projects, :users, column: :video_user_id
+    add_foreign_key :projects, :users, column: :manage_usre_id
   end
 end
